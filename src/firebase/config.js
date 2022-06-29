20d6636bc9449cc49503cacdefba036aae54a62b
J0kk3 <joakim_88@msn.com>
2022-06-29 08:44:16 +0200

    [ADD] firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore"
+import "firebase/compat/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-LZEe2tKOKlB_oh9--OwtJdLzU472EeU",
+    authDomain: "mymoney-85928.firebaseapp.com",
+    projectId: "mymoney-85928",
+    storageBucket: "mymoney-85928.appspot.com",
+    messagingSenderId: "391844684841",
+    appId: "1:391844684841:web:9627b039337cd26fd1a838"
+};
+
+// Init firebase
+firebase.initializeApp( firebaseConfig );
+
+// Init Service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
