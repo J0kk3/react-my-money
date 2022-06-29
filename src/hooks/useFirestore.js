49ff57cb2cd3292b99f9c8a1b9920ac4e7dc28b6
J0kk3 <joakim_88@msn.com>
2022-06-29 23:05:38 +0200

    [ADD] deleting transactions

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -27,6 +27,13 @@ const firestoreReducer = ( state, action ) =>
                 success: true,
                 error: null
             }
+        case "DELETED_DOCUMENT":
+            return {
+                isPending: false,
+                document: null,
+                success: true,
+                error: null
+            }
         case "ERROR":
             return {
                 isPending: false,
@@ -79,14 +86,21 @@ export const useFirestore = ( collection ) =>
     //Delete a document
     const deleteDocument = async ( id ) =>
     {
+        dispatch( { type: "IS_PENDING" } );
+        try
+        {
+            await ref.doc( id ).delete();
+            dispatchIfNotCancelled( { type: "DELETED_DOCUMENT" } );
+        }
+        catch ( err )
+        {
+            dispatchIfNotCancelled( { type: "ERROR", payload: "Could not delete" } );
+        }
     }
 
     useEffect( () =>
     {
-        return () =>
-        {
-            setIsCancelled( true );
-        }
+        return () => setIsCancelled( true );
     }, [] );
 
     return { addDocument, deleteDocument, response };
--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,15 +1,19 @@
-import React from "react";
+import { useFirestore } from "../../hooks/useFirestore";
 //Styles
 import styles from "./Home.module.css";
 
 export default function TransactionList ( { transactions } )
 {
+    const { deleteDocument, response } = useFirestore( "transactions" );
+    console.log( response );
+
     return (
         <ul className={ styles.transactions }>
             { transactions.map( ( transaction ) => (
                 <li key={ transaction.id }>
                     <p className={ styles.name }>{ transaction.name }</p>
                     <p className={ styles.amount }>{ transaction.amount }skr</p>
+                    <button onClick={ () => deleteDocument( transaction.id ) }>x</button>
                 </li>
             ) ) }
         </ul>
