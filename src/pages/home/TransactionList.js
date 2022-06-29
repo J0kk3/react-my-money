3808dfe3a240c0e695395820905f2326c6dd271c
J0kk3 <joakim_88@msn.com>
2022-06-29 23:55:43 +0200

    [ADD] timestamp to transactionList

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -7,11 +7,27 @@ export default function TransactionList ( { transactions } )
     const { deleteDocument, response } = useFirestore( "transactions" );
     console.log( response );
 
+    let date;
+
+    let options =
+    {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    transactions.forEach( transaction =>
+    {
+        date = transaction.createdAt.toDate().toLocaleDateString( "en-US", options );
+    } )
+
     return (
         <ul className={ styles.transactions }>
             { transactions.map( ( transaction ) => (
                 <li key={ transaction.id }>
                     <p className={ styles.name }>{ transaction.name }</p>
+                    <p className={ styles.date }>{ date }</p>
                     <p className={ styles.amount }>{ transaction.amount }skr</p>
                     <button onClick={ () => deleteDocument( transaction.id ) }>x</button>
                 </li>
